79c4b90d90d26d8aba4b468307cf3b92e6b53572
Santhoshraj263 <88678528+Santhoshraj263@users.noreply.github.com>
2025-06-26 18:39:40 +0530

    Update myscript.sh

--- myscript.sh
+++ myscript.sh
@@ -4,7 +4,7 @@
 # It explains what the script does.
 
 echo "Hello, world!"
-echo "This is a simple shell script!!."
+echo "This is a simple shell script!."
 
 # You can also display the output of commands.
 echo "Current date and time:"

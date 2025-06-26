6f8101658864a8fe72c4c746ae0cd3dbde2cf06a
Santhoshraj263 <88678528+Santhoshraj263@users.noreply.github.com>
2025-06-26 17:40:22 +0530

    Create myscript.sh

---
+++ myscript.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# This is a comment, ignored by the shell.
+# It explains what the script does.
+
+echo "Hello, world!"
+echo "This is a simple shell script."
+
+# You can also display the output of commands.
+echo "Current date and time:"
+date
+
+echo "Your current working directory is:"
+pwd

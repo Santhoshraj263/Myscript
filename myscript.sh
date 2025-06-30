893bd64fd7c9ca2ddc47d2a539c3d20d5f738908
Santhoshraj263 <88678528+Santhoshraj263@users.noreply.github.com>
2025-06-30 17:51:42 +0530

    Update myscript.sh

--- myscript.sh
+++ myscript.sh
@@ -3,11 +3,11 @@
 # This is a comment, ignored by the shell.
 # It explains what the script does.
 
-ec"Hello, world!"
-ec"This is a simple shell script!."
+echo "Hello, world!"
+echo "This is a simple shell script!."
 
 # You can also display the output of commands.
-ec"Current date and time:"
+echo "Current date and time:"
 date
 
 echo "Your current working directory is:"
